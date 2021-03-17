11389e008eb5195a7476fd6538524143cbb6c09e
bromowi <bromo.wibowo91@gmail.com>
2021-03-17 16:12:25 +0700

    fetching data

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,54 @@
-import react from "react";
-function Home () {
-    return(
-        <h1>ini home lo</h1>
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import fetchJsonp from 'fetch-jsonp';
+import { camelizeKeys } from 'humps';
+
+
+const Character = () => {
+    const [states, setStates] = useState([]);
+    const getData = async () => {
+        let result = await fetchjson();
+        console.log(result)
+    };
+
+    const fetchjson = () => {
+        fetchJsonp( 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&tagmode=any', {
+            jsonpCallback: 'jsoncallback',
+            
+          }).then(response => { 
+              return response.json()
+        })
+
+    };
+
+    const camelizeJSON = json =>
+    camelizeKeys(json, (key, convert) => {
+      return /^[A-Z0-9_]+$/.test(key) ? key : convert(key);
+    });
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    console.log(states, "ayo maniis");
+
+    return (
+        <div>
+            {states?.map((data, idx) => {
+                return (
+                    <div key={idx}>                          
+                        <div>
+                            <img src= {data.picture.medium} width='200'/>
+                            <p>Name: {data.name.first} {data.name.last}</p>
+                            <p>Realease: {data.nat} </p>
+                            <br />
+                            <br />
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
-}
-export default Home
+
+        }
+export default Character;
